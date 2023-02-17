142b1a08b4048ecd1b9b0f5f017d54aa6e672f2e
ayoshinin <ayodejisamson73@yahoo.com>
2023-02-16 21:02:59 -0800

    i input number

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main- A program in c that prints a line in standard error.
- * Return: 1 on sucess
+ * main- A program in c that run a standard error.
+ * Return: i on sucess.
  */
 int main(void)
 {
- fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
- return (1);
-}
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
