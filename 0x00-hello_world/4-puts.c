9208b4ed5355e1424278e467cffd3618274ab678
ayoshinin <ayodejisamson73@yahoo.com>
2023-02-16 19:23:18 -0800

    lines of input function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- It is a primary point entry for puts function
+ *
+ * Return: By default it return zero if no errors it return non-zero value and it is a sucess.
+ */
+int main(void)
+{
+ puts("\"progtamming is like building a multilingual puzzle");
+ return (0);
+}
