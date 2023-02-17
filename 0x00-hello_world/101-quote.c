a308a50560938fb304cd25cad086244ee2e70dc6
ayoshinin <ayodejisamson73@yahoo.com>
2023-02-16 20:53:59 -0800

    simple quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- A program in c that prints a line in standard error.
+ * Return: 1 on sucess
+ */
+int main(void)
+{
+ fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+ return (1);
+}
