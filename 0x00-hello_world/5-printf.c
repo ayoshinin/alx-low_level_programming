c53a1f7079d194ca8fcb0d32008b2277de0e9837
ayoshinin <ayodejisamson73@yahoo.com>
2023-02-16 19:46:29 -0800

    printing function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- A program that write lines in a print function.
+ * Return: It return zero by default if the function is a sucess.
+ */
+int main(void)
+{
+print("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
