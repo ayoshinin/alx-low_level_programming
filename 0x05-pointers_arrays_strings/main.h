385c71d51cf47187e3dc9c3d114a870e970b4cd3
ayoshinin <ayodejisamson73@yahoo.com>
2023-02-27 08:13:11 -0800

    number base

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * prototypes files
- * import form here
- */
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -19,3 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#endif /* MAIN_H */
