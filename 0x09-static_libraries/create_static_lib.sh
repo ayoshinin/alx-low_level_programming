4b3a90887ee43a735d287bdf7cd81a2b1b236296
ayoshinin <ayodejisamson73@yahoo.com>
2023-03-10 20:44:54 -0800

    acheiving my goal

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,10 @@
-i#!/bin/bash
+#!/bin/bash
+
+# a script that creates a static library
+# called liball.a from all the .c files
+# that are in the current directory
+
 gcc -c *.c
 ar rc liball.a *.o
-ranlib liball.a
+echo y | rm -I *.o
+
