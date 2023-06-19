50e1df13c7d7e8010e1cb9f4e03adeab3958083b
ayoshinin <ayodejisamson73@yahoo.com>
2023-06-19 09:54:50 -0700

    count line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - It helps to print the name of a program file when complied,
+ *        and it follows a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
